6d677d16f88e05cebb0fbf5a878377b1ddae954e
dy93 <dy93_@hotmail.com>
2017-12-09 15:45:42 +0800

    Setup react-toolbox demo page

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import Button from 'react-toolbox/lib/button/Button';
+import TimePicker from 'react-toolbox/lib/time_picker/TimePicker';
 import logo from './logo.svg';
 import './App.css';
 
@@ -8,11 +10,13 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to React ToolBox</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <Button raised primary>This is RT!!</Button>
+        <TimePicker >kerker</TimePicker>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ThemeProvider from 'react-toolbox/lib/ThemeProvider';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import theme from './assets/react-toolbox/theme';
+import './assets/react-toolbox/theme.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<ThemeProvider theme={theme}><App /></ThemeProvider>, document.getElementById('root'));
 registerServiceWorker();
